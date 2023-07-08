bb3ccd2fe461ca64d035ae1012aedbdabf0008f6
Zeeshan Yousaf <zeeshanento34@gmail.com>
2023-07-08 10:05:06 +0500

    Navbar updated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,22 @@
 import React from "react";
 import { Box, Link, Typography, useTheme } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
 const Navbar = () => {
   const theme = useTheme();
+  const navigate = useNavigate();
+  const loggedIn = JSON.parse(localStorage.getItem("authToken"));
+  const handleLogout = async () => {
+    try {
+      await axios.post("/api/v1/auth/logout");
+      localStorage.removeItem("authToken");
+      toast.success("logout successfully ");
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <Box
@@ -15,12 +29,25 @@ const Navbar = () => {
         <Typography variant="h1" color="primary" fontWeight="bold">
           AI GPT3 Clone
         </Typography>
-        <Link href="/register" p={1}>
-          Sign Up
-        </Link>
-        <Link href="/login" p={1}>
-          Sign in
-        </Link>
+        {loggedIn ? (
+          <>
+            <NavLink to="/" p={1}>
+              Home
+            </NavLink>
+            <NavLink to="/login" onClick={handleLogout} p={1}>
+              Logout
+            </NavLink>
+          </>
+        ) : (
+          <>
+            <NavLink to="/register" p={1}>
+              Sign Up
+            </NavLink>
+            <NavLink to="/login" p={1}>
+              Sign In
+            </NavLink>
+          </>
+        )}
       </Box>
     </>
   );
